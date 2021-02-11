bdd7c2b15145b8d3c65311fdca12925a118931ea
prathamrana <ranapratham1@gmail.com>
2021-02-11 12:23:46 -0500

    FlowControlHW

---
+++ operators.php
@@ -0,0 +1,13 @@
+<?php
+echo "<br>\nPHP Exercise [Operators] Exercise [1]<br>\n";
+echo 10 * 5;
+
+echo "<br>\nPHP Exercise [Operators] Exercise [2]<br>\n";
+echo 10 / 2;
+
+echo "<br>\nPHP Exercise [Operators] Exercise [3]<br>\n";
+var_dump($a == $b);
+
+echo "<br>\nPHP Exercise [Operators] Exercise [4]<br>\n";
+var_dump($a != $b);
+?>
