07c01dfee36b36e5811bf36c7bad6a20df77aa7c
prathamrana <ranapratham1@gmail.com>
2021-04-05 15:40:27 -0400

    Added Table for Account

---
+++ project/test/test_edit_accounts.php
@@ -0,0 +1,69 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
+if (!has_role("Admin")) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    flash("You don't have permission to access this page");
+    die(header("Location: login.php"));
+}
+?>
+<?php
+//we'll put this at the top so both php block have access to it
+if(isset($_GET["id"])){
+    $id = $_GET["id"];
+}
+?>
+<?php
+//saving
+if(isset($_POST["save"])){
+    $account_number = $_POST["account_number"];
+    $account_type = $_POST["account_type"];
+    $balance = $_POST["balance"];
+    $db = getDB();
+    $user = get_user_id();
+    if(isset($id)){
+        $stmt = $db->prepare("UPDATE Accounts set account_number=:account_number, account_type=:account_type, balance=:balance where id=:id");
+        $r = $stmt->execute([
+            ":account_number"=> $account_number,
+            ":account_type"=>$account_type,
+            ":balance"=>$balance,
+            ":id"=>$id
+        ]);
+        if($r){
+            flash("Updated successfully with id: " . $id);
+        }
+        else{
+            $e = $stmt->errorInfo();
+            flash("Error updating: " . var_export($e, true));
+        }
+    }
+    else{
+        flash("ID isn't set, we need an ID in order to update");
+    }
+}
+?>
+<?php
+//fetching
+$result = [];
+if(isset($id)){
+    $id = $_GET["id"];
+    $db = getDB();
+    $stmt = $db->prepare("SELECT * FROM Accounts where id = :id");
+    $r = $stmt->execute([":id"=>$id]);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+}
+?>
+
+<form method="POST">
+    <label>Account Number</label>
+    <input type="number" name="account_number" value="<?php echo $result["account_number"];?>"/>
+    <label>Account Type</label>
+    <select name="account_type" value="<?php echo $result["account_type"];?>">
+        <option value="checking" <?php echo ($result["account_type"] == "0"?'selected="selected"':'');?>>checking</option>
+        <option value="saving" <?php echo ($result["account_type"] == "2"?'selected="selected"':'');?>>saving</option>
+    </select>
+    <label>Balance</label>
+    <input type="number" min="1.00" name="balance" value="<?php echo $result["balance"];?>" />
+    <input type="submit" name="save" value="Update"/>
+</form>
+
+<?php require(__DIR__ . "/partials/flash.php");
