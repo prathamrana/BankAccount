6faf4573e7244a7c6dd4f24768c1e7dbafcb6b5a
prathamrana <ranapratham1@gmail.com>
2021-05-01 17:52:44 -0400

    Update

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -12,20 +12,11 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <?php endif; ?>
 
         <?php if (has_role("Admin")): ?>
-            <li><a href="<?php echo getURL("test/test_create_accounts.php");?>">Create Account</a></li>
-            <li><a href="<?php echo getURL("test/test_list_accounts.php");?>">List Accounts</a></li>
-            <li><a href="<?php echo getURL("test/test_create_transactions.php");?>">Create Transaction</a></li>
-            <li><a href="<?php echo getURL("test/test_list_transactions.php");?>">List Transactions</a></li>
+            <li><a href="test_create_accounts.php">Create Account</a></li>
+            <li><a href="test_list_accounts.php">List Accounts</a></li>
+            <li><a href="test_create_transactions.php">Create Transaction</a></li>
+            <li><a href="test_list_transactions.php">List Transactions</a></li>
         <?php endif; ?>
-
-        <?php if (is_logged_in()): ?>
-            <li><a href="newaccount.php">Open an Account</a></li>
-            <li><a href="listaccounts.php">View Accounts</a></li>
-            <li><a href="dw.php">Deposit or Withdraw</a></li>
-            <li><a href="myaccttransfer.php">Account Transfer</a></li>
-            <li><a href="transfer.php">Transfer</a></li>
-            <li><a href="profile.php">Profile</a></li>
-            <li><a href="logout.php">Logout</a></li>
-        <?php endif; ?>
+        
     </ul>
 </nav>
