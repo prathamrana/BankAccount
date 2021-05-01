c9145dbda6b11016bf2ccaa04c6ad524b213cf48
prathamrana <ranapratham1@gmail.com>
2021-05-01 18:05:08 -0400

    Updated

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -17,6 +17,17 @@ require_once(__DIR__ . "/../lib/helpers.php");
             <li><a href="test_create_transactions.php">Create Transaction</a></li>
             <li><a href="test_list_transactions.php">List Transactions</a></li>
         <?php endif; ?>
+
+        <?php if (is_logged_in()): ?>
+        <li><a href="newaccount.php">Open an Account</a></li>
+        <li><a href="listaccounts.php">View Accounts</a></li>
+        <li><a href="dw.php">Deposit or Withdraw</a></li>
+        <li><a href="myaccttransfer.php">Account Transfer</a></li>
+        <?php endif; ?>
         
+        <?php if (is_logged_in()): ?>
+            <li><a href="profile.php">Profile</a></li>
+            <li><a href="logout.php">Logout</a></li>
+        <?php endif; ?>
     </ul>
 </nav>
